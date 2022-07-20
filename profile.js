9307ecef759029c84bea6b9cfea097b2aef5a429
Shivesh Nandan <96003262+ShiveshNandan@users.noreply.github.com>
2022-07-20 13:34:58 +0530

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var $window = $(window);
+
+function run()
+{
+	var fName = arguments[0],
+		aArgs = Array.prototype.slice.call(arguments, 1);
+	try {
+		fName.apply(window, aArgs);
+	} catch(err) {
+		 
+	}
+};
+ 
+/* chart
+================================================== */
+function _chart ()
+{
+	$('.b-skills').appear(function() {
+		setTimeout(function() {
+			$('.chart').easyPieChart({
+				easing: 'easeOutElastic',
+				delay: 3000,
+				barColor: '#041d54',
+				trackColor: 'rgba(235, 220, 220, 0)',
+				scaleColor: false,
+				lineWidth: 21,
+				trackWidth: 21,
+				size: 250,
+				lineCap: 'round',
+				onStep: function(from, to, percent) {
+					this.el.children[0].innerHTML = Math.round(percent)/10;
+					// this.el.children[0].innerHTML = ;
+
+				}
+			});
+		}, 150);
+	});
+};
+ 
+
+$(document).ready(function() {
+  
+	run(_chart);
+  
+    
+});
